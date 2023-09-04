3c1f9f9ff658ab9170e6bd1a31cea6317e7e71cb
Mikhail-boosta <mikhail.obrazenko@boosta.co>
2023-09-04 11:07:23 +0300

    base

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-import {fetchDataWithAppend} from "./api/api-get-json.js";
-import {setCurrentToggle} from "./ui/ui-actions/toggle.js";
-import {filterSwitcher} from "./ui/ui-actions/switcher.js";
-import {executeButton} from "./ui/ui-actions/button.js";
-import {favouriteAdd} from "./ui/ui-actions/favourite.js";
-import {appendMessagesList} from "./ui/html-appends/message-list-append.js";
-import {activateBurgerMenu} from "./ui/ui-actions/burger.js";
-import {favouriteMessages} from "./ui/ui-elements.js";
-import {storedObjectArray} from "./store/favourite-store.js";
+import {fetchDataWithAppend} from "api/api-get-json.js";
+import {setCurrentToggle} from "ui/ui-actions/toggle.js";
+import {filterSwitcher} from "ui/ui-actions/switcher.js";
+import {executeButton} from "ui/ui-actions/button.js";
+import {favouriteAdd} from "ui/ui-actions/favourite.js";
+import {appendMessagesList} from "ui/html-appends/message-list-append.js";
+import {activateBurgerMenu} from "ui/ui-actions/burger.js";
+import {favouriteMessages} from "ui/ui-elements.js";
+import {storedObjectArray} from "store/favourite-store.js";
 
 
 window.onload = () => {
